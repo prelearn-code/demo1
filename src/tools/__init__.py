e6608bcdf2cdba91cd83cc267519f3e088b05a29
prelearn-code <prelearn@qq.com>
2026-01-09 10:40:07 +0800

    000

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,8 +1,13 @@
+
+
 """
 工具层
 包含代码执行和输出解析功能
 """
+
+# 让这个文件成为一个包
 from .sandbox import execute_code
 from .parser import extract_code
 
+# 创建一个python导入的白名单
 __all__ = ["execute_code", "extract_code"]
